d8c54eedd6812b012133acc22b611f5bd4ffa933
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-11-03 21:12:20 -0300

    feat: ajust css

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+      {children}
+    </div>
+  )
 }
