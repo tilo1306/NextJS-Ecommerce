8a5ee09df5b121c5d81f4a2d6e1a6f9dadce4196
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-11-03 21:13:37 -0300

    chore: add lucide-react

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+
+        </form>
+      </div>
+      <div className="flex items-center gap-4"> </div>
+    </div>
+  )
 }
