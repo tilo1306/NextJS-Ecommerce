471422ab35585b9e2962418e289b699ea6afbbaa
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-11-09 23:10:20 -0300

    feat: Fetch de produtos na Home

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,8 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const apiPrefic = '/api'
 
-  const url = new URL(path, baseUrl)
+  const url = new URL(apiPrefic.concat(path), baseUrl)
   return fetch(url, init)
 }
