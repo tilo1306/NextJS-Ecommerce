a0c10b3cc842a3d13038519441120436a8d5181f
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-11-29 18:30:14 -0300

    test: search

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,11 @@
-describe('Add product to cart', () => {
+describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
     cy.get('a[href^="/product"]').first().click()
+
     cy.location('pathname').should('include', '/product')
     cy.contains('Adicionar ao carrinho').click()
 
@@ -9,8 +13,6 @@ describe('Add product to cart', () => {
   })
 
   it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('a[href^="/product"]').first().click()
 
     cy.location('pathname').should('include', '/product')
@@ -21,8 +23,6 @@ describe('Add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('input[name=q]').type('moletom').parent('form').submit()
 
     cy.get('a[href^="/product"]').first().click()
